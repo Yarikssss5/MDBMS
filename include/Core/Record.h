e435b88ee39962db7b69811598db863ca1c8725f
Yarikssss5 <yariksemenovys@yandex.ru>
2024-07-31 15:07:01 +0500

    Writed implemetation of change_size for Record and Table

--- include/Core/Record.h
+++ include/Core/Record.h
@@ -9,11 +9,10 @@ typedef struct {
 	size_t length;
 	size_t top;
 	size_t id;
-	Type *template;
 } Record;
 
 
-Record *new_Record() {
+inline Record *new_Record() {
 	Record *tmp = (Record*)malloc(sizeof(Record));
 	if (tmp != NULL) {
 		tmp->data = NULL;
@@ -22,20 +21,31 @@ Record *new_Record() {
 }
 
 
-void remove_Record(Record *record) {
+inline void remove_Record(Record *record) {
 	if (record != NULL) {
 		if (record->data != NULL) free(record->data);
-		if (record->template != NULL) free(record->template);
 		free(record);
 	}
 }
 
 
-bool change_size_Record(Record *record, size_t length) {
+inline bool change_size_Record(Record *record, size_t length) {
 	if (record != NULL ) {
 		Field *tmp = (Field*)malloc(sizeof(Field) * length);
 		if (tmp == NULL) return false;
-		
+        if (record->length != 0) {
+            for (int i = 0; i < (int)record->length; i++) 
+                tmp[i] = record->data[i];
+            free(record->data);
+            record->data = tmp;
+            record->length = length;
+            return true;
+        }
+        else {
+            record->data = tmp;
+            record->length = length;
+            return true;
+        }
 	}
 	else return false;
 }
--- include/Core/Table.h
+++ include/Core/Table.h
@@ -13,12 +13,38 @@ typedef struct {
 } Table;
 
 
-Table *new_Table() {
+inline Table *new_Table() {
 	Table *tmp = (Table*)malloc(sizeof(Table));
 	if (tmp != NULL) {
-		
+        tmp->data = NULL;
+        tmp->length = 0;
+        tmp->name = *empty_MyString();
 	}
 	return tmp;
+}
+
+
+inline bool change_size_Table(Table *table, size_t new_size) {
+    if (table != NULL) {
+        Record *tmp = (Record*)malloc(sizeof(Record) * new_size);
+        if (tmp != NULL) {
+            if (table->length != 0) {
+                for (int i = 0; i < (int)table->length; i++) 
+                    tmp[i] = table->data[i];
+                free(table->data);
+                table->data = tmp;
+                table->length = new_size;
+                return true;
+            }
+            else {
+                table->data = tmp;
+                table->length = new_size;
+                return true;
+            }
+        }
+        else return false;
+    }
+    else return false;
 }
 
 
